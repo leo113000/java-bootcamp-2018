a1fb4d5a67dbe8ce5502e2c44829fb4d403a7b47
Leo <leo114000@gmail.com>
2018-05-20 19:35:06 -0300

    User Repository queries

--- src/main/java/com/globant/bootcamp/persistence/UserRepository.java
+++ src/main/java/com/globant/bootcamp/persistence/UserRepository.java
@@ -2,8 +2,16 @@ package com.globant.bootcamp.persistence;
 
 import com.globant.bootcamp.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface UserRepository extends JpaRepository<User,Long> {
+@Repository public interface UserRepository extends JpaRepository<User, Long> {
+
+	@Query("select u from User u where u.username = :username")
+	User findByUsername(@Param("username")String username);
+
+	@Query("select u from User u where u.email = :email")
+	User findByEmail(@Param("email")String email);
+
 }
