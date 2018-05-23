c891b641322f8f1dfb77f4e73e2c238a6cf0fcb7
Leo <leo114000@gmail.com>
2018-05-22 23:10:36 -0300

    Order and OrderLines

--- src/main/java/com/globant/bootcamp/model/Order.java
+++ src/main/java/com/globant/bootcamp/model/Order.java
@@ -6,18 +6,21 @@ import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.Date;
+import java.util.List;
 
 @NoArgsConstructor @Getter @Setter @Entity @Table(name = "orders") public class Order {
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY) @Column(name = "id") private Long id;
-	@OneToOne @JoinColumn(name = "cart_id") private Cart cart;
+	@Getter @Setter @OneToOne @JoinColumn(name = "user_id") private User user;
 	@Column(name = "date") private Date date;
 	@Column(name = "total") private double total;
 	@ManyToOne @JoinColumn(name = "deliver_method_id") private DeliveryMethod deliveryMethod;
 	@ManyToOne @JoinColumn(name = "payment_method_id") private PaymentMethod paymentMethod;
 	@ManyToOne @JoinColumn(name = "status_id") private Status status;
 
-	public Order(Cart cart, Date date, DeliveryMethod deliveryMethod, PaymentMethod paymentMethod, Status status) {
-		this.cart = cart;
+	@Getter @Setter @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, fetch = FetchType.EAGER) private List<OrderLine> orderLines;
+
+	public Order(User user, Date date, DeliveryMethod deliveryMethod, PaymentMethod paymentMethod, Status status) {
+		this.user = user;
 		this.date = date;
 		this.deliveryMethod = deliveryMethod;
 		this.paymentMethod = paymentMethod;
--- src/main/java/com/globant/bootcamp/model/OrderLine.java
+++ src/main/java/com/globant/bootcamp/model/OrderLine.java
@@ -1,4 +1,27 @@
 package com.globant.bootcamp.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
 public class OrderLine {
+	@Getter @Setter @Id @GeneratedValue(strategy = GenerationType.AUTO) @Column(name = "id") private String id;
+
+	@Getter @Setter @ManyToOne @JoinColumn(name = "product_id") private Product product;
+
+	@Getter @Setter @Column(name = "qty") private int qty;
+	@Setter @Column(name = "subtotal") private double subtotal;
+
+	@ManyToOne @JoinColumn(name = "order_id") private Order order;
+
+	public OrderLine(Product product, int qty) {
+		this.product = product;
+		this.qty = qty;
+	}
+
+	public double getSubtotal(){
+		this.subtotal = product.getPrice() * qty;
+		return this.subtotal;
+	}
 }
